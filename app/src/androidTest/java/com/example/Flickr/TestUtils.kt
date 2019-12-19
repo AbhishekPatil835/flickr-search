b90e863fc19622772b40fc0fd4555ee1a48da37a
Syed Musaddiq Faraz <musaddiq.faraz1@gmail.com>
2019-12-19 20:45:05 +0530

    added  pagelist mock function, and instrumentation testcase to verify page list data.

---
+++ app/src/androidTest/java/com/example/Flickr/TestUtils.kt
@@ -0,0 +1,6 @@
+package com.example.Flickr
+
+import com.example.Flickr.data.Photo
+
+val photo = Photo("id","owner","secret","server",1,"Title",1,0,0)
+val photoList = arrayListOf(photo, photo, photo)
